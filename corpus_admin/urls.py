79aa8e2c83fa04cbbc165ae0fb7ff6f826b9cfef
Diego Alberto Barriga Martínez <diegobarriga@protonmail.com>
2020-05-22 12:28:35 -0500

    add(export-database): Botón en corpus admin
    se agrega botón en el administrador del corpus para exportar la base de
    datos de elasticsearch. Close #20

--- corpus_admin/urls.py
+++ corpus_admin/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('preview/<str:_id>', views.doc_preview, name="preview"),
     path('edit/<str:_id>', views.doc_edit, name='edit'),
     path('add/<str:_id>', views.add_doc_data, name='add'),
-    path('delete/', views.delete_doc, name='delete')
+    path('delete/', views.delete_doc, name='delete'),
+    path('export-corpus-data/', views.export_data, name="export-corpus-data")
 ]
