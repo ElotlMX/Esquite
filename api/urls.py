7c9991db0dd9303738c27e575e7f759bb6a63aa8
Diego Alberto Barriga Martínez <diegobarriga@protonmail.com>
2020-10-21 21:34:08 -0500

    refactor(api): Endpoints and logic
    Index name must be in the json request to the api's endpoints.
    Adding error handling to endpoints. Change returned results
    depending on user type. Adding Doc Strings to the views. Close #73

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-    path('', views.info, name='api-info'),
     path('search/', views.basic_search, name='basic_search'),
     path('full-search/', views.full_search, name='full_search'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
